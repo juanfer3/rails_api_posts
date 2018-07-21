4ffdc4197412a03fd10eaced84d15699842327a6
juanfer3 <jfvilladiego2@misena.edu.co>
2018-07-20 22:19:44 -0500

    Api

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+ 
+=begin
+ devise :database_authenticatable, 
+        :registerable,
+        :recoverable, 
+        :rememberable, 
+        :trackable,  
+        :validatable, 
+        :omniauthable
+=end
   include DeviseTokenAuth::Concerns::User
-  
+  before_create :skip_confirmation!
 end
